fc1f6b7a8a2f79b0a4d4f5c1700a6c84757cd5c5
EC2 Default User <ec2-user@ip-172-31-43-89.ap-northeast-1.compute.internal>
2023-09-03 04:07:51 +0000

    rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_03_023933) do
+ActiveRecord::Schema.define(version: 2021_07_27_061455) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
 end
